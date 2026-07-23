9eb6b94b2044fdcb2b5d7a8a81957885aa32c8ae
windzer99311 <windzergamer@gmail.com>
2026-07-23 12:14:04 +0530

    Update video_downloader.py

--- video_downloader.py
+++ video_downloader.py
@@ -1,80 +1,15 @@
-import os, re,subprocess, requests, time, streamlit as st
+import os, subprocess, requests, time, streamlit as st
+from pytubefix import YouTube
+from pytubefix.exceptions import RegexMatchError
 from math import ceil, floor
 from concurrent.futures import ThreadPoolExecutor
 
-video_segment_list, audio_segment_list, raw_video_option_list, raw_audio_option_list, displaying_vid_resolution, displaying_aud_resolution, Reso_check = [], [], [], [], [], [], []
+video_segment_list, audio_segment_list, Reso_list, Aud_list, vid_resolution, aud_resolution, Reso_check = [], [], [], [], [], [], []
 video_file, audio_file, output_file = "Video.mp4", "song.mp3", 'Downloaded.mp4'
 only_audio = []
 total_received, percentage = 0, 0
-displaying_quality_list=[]
-raw_quality_list=[]
-default_lowest_audio=[]
-
-def video_response_scraper(video_id):
-    endpoint_url = "https://www.youtube.com/youtubei/v1/player?prettyPrint=false"
-
-    headers = {
-        "User-Agent": "com.google.android.apps.youtube.vr.oculus/1.60.19 (Linux; U; Android 12L; eureka-user Build/SQ3A.220605.009.A1) gzip",
-        "accept-language": "en-US,en",
-        "Content-Type": "application/json",
-        "X-Youtube-Client-Name": "28"
-    }
-
-    data_1 = {
-        "context": {
-            "client": {
-                "clientName": "WEB",
-                "osName": "Windows",
-                "osVersion": "10.0",
-                "clientVersion": "2.20250523.01.00",
-                "platform": "DESKTOP"
-            }
-        },
-        "videoId": video_id,
-        "contentCheckOk": True
-    }
-
-    data_2 = {
-        "context": {
-            "client": {
-                "clientName": "ANDROID_VR",
-                "clientVersion": "1.60.19",
-                "deviceMake": "Oculus",
-                "deviceModel": "Quest 3",
-                "osName": "Android",
-                "osVersion": "12L",
-                "androidSdkVersion": "32"
-            }
-        }
-    }
-
-    visitor_data="Cgs4OWNkTXlSTS1Ucyi-vrHNBjIKCgJJThIEGgAgOg%3D%3D"
-
-    # Update second payload
-    data_2["context"]["client"]["visitorData"] = visitor_data
-    data_2.update(dict(videoId=video_id, contentCheckOk=True))
-    # Second request
-    r2 = requests.post(endpoint_url, headers=headers, json=data_2).json()
-    if r2["playabilityStatus"]["status"] == "OK":
-        pass
-    else:
-        #callback if visitorData Expires:
-        r1 = requests.post(endpoint_url, headers=headers, json=data_1)
-        visitor_response = r1.json()
-        visitor_data = visitor_response["responseContext"]["visitorData"]
-        data_2["context"]["client"]["visitorData"] = visitor_data
-        data_2.update(dict(videoId=video_id, contentCheckOk= True))
-        r2 = requests.post(endpoint_url, headers=headers, json=data_2).json()
-    return r2
-
-def get_video_id(url):
-    # Regex pattern to match various YouTube URL formats
-    pattern = r'(?:v=|\/|be\/|embed\/|shorts\/)([0-9A-Za-z_-]{11})'
-
-    match = re.search(pattern, url)
-    if match:
-        return match.group(1)
-    return None
+Resolution_list=[]
+Raw_rso_list=[]
 
 def create_throttles(size):
     mb = 1024 * 1024
@@ -105,15 +40,11 @@ def range_list(file_size, parts, file_type):
                 audio_segment_list.append((start, end))
         start = end + 1
 
-def metadata(video_details):
-    thumbnail= video_details["videoDetails"]["thumbnail"]["thumbnails"][2]["url"]
-    title= video_details["videoDetails"]["title"]
-    views= video_details["videoDetails"]["viewCount"]
-    total_seconds= int(video_details["videoDetails"]["lengthSeconds"])
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-    seconds = total_seconds % 60
-    duration = f"{hours:02}:{minutes:02}:{seconds:02}"
+def metadata(stream_data, yt):
+    thumbnail = yt.thumbnail_url
+    title = stream_data.first().title
+    views = yt.views
+    duration = time.strftime("%H:%M:%S", time.gmtime(yt.length))
     return thumbnail, title, views, duration
 
 def track_progress(data_received, total_length):
@@ -157,11 +88,10 @@ def merge(video_data, audio_data):
     os.remove(audio_data)
     st.success("Video Processed Successfully!!")
 
-def setting_display_resolution(raw_list, new_list):
+def store_resolution(raw_list, new_list):
     for i in range(len(raw_list)):
         data = f"{raw_list[i][0]} {str(round(raw_list[i][2] / (1024 * 1024), 2))}  mb"
         new_list.append(data)
-    print(new_list)
 
 def Audio_Ui():
     st.title("🎵 Only Audio Downloader 🎵")
@@ -175,23 +105,19 @@ def Audio_Ui():
         st.session_state.checked_1 = True
     if st.session_state.get("checked_1", False):
         try:
-            video_id = get_video_id(link)
-            if video_id is None:
-                raise ValueError("Invalid URL")
-            details = video_response_scraper(video_id)
-            for i in details["streamingData"]["adaptiveFormats"]:
-                try:
-                    if "audio" in i["mimeType"]:
-                        raw_audio_option_list.append((f"{round(int(i['bitrate']) / 1000)}kbps", i["url"], int(i["contentLength"]), i["mimeType"]))
-                except:
-                    pass
-            vid_info = metadata(details)
-            setting_display_resolution(raw_audio_option_list, displaying_aud_resolution)
+            yt = YouTube(link)
+            streams = yt.streams
+            for stream in streams.filter(only_audio=True):
+                audio_resolution = stream.abr, stream.itag, stream.filesize, stream.audio_codec
+                Aud_list.append(audio_resolution)
+            vid_info = metadata(streams, yt)
+            print(Aud_list)
+            store_resolution(Aud_list, aud_resolution)
             col1, col2 = st.columns(2)
             with col1:
                 st.image(f"{vid_info[0]}", width=300)
-                a = st.selectbox("Audio Resolution", displaying_aud_resolution)
-                audio_resolution = raw_audio_option_list[displaying_aud_resolution.index(a)]
+                a = st.selectbox("Audio Resolution", aud_resolution)
+                audio_resolution = Aud_list[aud_resolution.index(a)]
 
             with col2:
                 st.subheader(f"Title: {vid_info[1]}")
@@ -199,7 +125,7 @@ def Audio_Ui():
                 download = st.button("Process")
 
             if download:
-                audio_stream = audio_resolution[1]
+                audio_stream = streams.filter().get_by_itag(audio_resolution[1]).url
                 total_size =audio_resolution[2]
                 approx_size = round(total_size / (1024 * 1024), 2)
                 print(f"File_Size: {approx_size}")
@@ -234,10 +160,10 @@ def Audio_Ui():
                         mime="audio/m4a"
                     )
                 os.remove(audio_file)
-        except ValueError:
+        except RegexMatchError:
             st.error("Please Enter Valid Url!")
 
-def youtube_gui():
+def Youtube_Gui():
     st.title("Youtube Video Downloader")
     inp1, inp2 = st.columns(2)
     with inp1:
@@ -249,22 +175,19 @@ def youtube_gui():
         st.session_state.checked_2 = True
     if st.session_state.get("checked_2", False):
         try:
-            video_id=get_video_id(link)
-            if video_id is None:
-                raise ValueError("Invalid URL")
-            main_response=video_response_scraper(video_id)
-            for i in main_response["streamingData"]["adaptiveFormats"]:
-                try:
-                    if "video" in i["mimeType"]:
-                        raw_video_option_list.append((i["qualityLabel"], i["url"], int(i["contentLength"]), i["fps"]))
-                    if "audio" in i["mimeType"]:
-                        raw_audio_option_list.append(
-                            (f"{round(int(i['bitrate']) / 1000)}kbps", i["url"], int(i["contentLength"]), i["mimeType"]))
-                except:
-                    pass
-            vid_info = metadata(main_response)
-            setting_display_resolution(raw_video_option_list, displaying_vid_resolution)
-            setting_display_resolution(raw_audio_option_list, displaying_aud_resolution)
+            yt = YouTube(link)
+            streams = yt.streams
+            for stream in streams.filter(only_video=True):
+                video_resolution = stream.resolution, stream.itag, stream.filesize, stream.fps
+                if f"{video_resolution[0]}  {str(video_resolution[3])}" not in Reso_check and video_resolution[0]!=None:
+                    Reso_list.append(video_resolution)
+                    Reso_check.append(f"{video_resolution[0]}  {str(video_resolution[3])}")
+            for stream in streams.filter(only_audio=True):
+                audio_resolution = stream.abr, stream.itag, stream.filesize, stream.audio_codec
+                Aud_list.append(audio_resolution)
+            vid_info = metadata(streams, yt)
+            store_resolution(Reso_list, vid_resolution)
+            store_resolution(Aud_list, aud_resolution)
 
             col1, col2 = st.columns(2)
             with col1:
@@ -275,16 +198,16 @@ def youtube_gui():
                 st.write(f"Duration:{vid_info[3]}")
             col3, col4 = st.columns(2)
             with col3:
-                selected_resolution = st.selectbox("Video Resolution", displaying_vid_resolution)
-                selected_video_detail_list = raw_video_option_list[displaying_vid_resolution.index(selected_resolution)]
+                v = st.selectbox("Video Resolution", vid_resolution)
+                video_resolution = Reso_list[vid_resolution.index(v)]
             with col4:
-                a = st.selectbox("Audio Resolution", displaying_aud_resolution)
-                selected_audio_detail_list = raw_audio_option_list[displaying_aud_resolution.index(a)]
+                a = st.selectbox("Audio Resolution", aud_resolution)
+                audio_resolution = Aud_list[aud_resolution.index(a)]
             download = st.button("Process")
             if download:
-                video_stream =selected_video_detail_list[1]
-                audio_stream = selected_audio_detail_list[1]
-                total_size = selected_video_detail_list[2] + selected_audio_detail_list[2]
+                video_stream = streams.filter().get_by_itag(video_resolution[1]).url
+                audio_stream = streams.filter().get_by_itag(audio_resolution[1]).url
+                total_size = video_resolution[2] + audio_resolution[2]
                 approx_size = round(total_size / (1024 * 1024), 2)
                 print(f"File_Size: {approx_size}")
                 st.header(f"Size:{approx_size}mb")
@@ -292,15 +215,15 @@ def youtube_gui():
                 downloading_status = st.empty()
 
                 with ThreadPoolExecutor() as executor:
-                    t1 = executor.submit(create_throttles, selected_video_detail_list[2])
-                    t2 = executor.submit(create_throttles, selected_audio_detail_list[2])
+                    t1 = executor.submit(create_throttles, video_resolution[2])
+                    t2 = executor.submit(create_throttles, audio_resolution[2])
 
                 video_throttles = t1.result()
                 audio_throttles = t2.result()
 
                 with ThreadPoolExecutor() as executor:
-                    executor.submit(range_list, selected_video_detail_list[2], video_throttles, "Video")
-                    executor.submit(range_list, selected_audio_detail_list[2], audio_throttles, "Audio")
+                    executor.submit(range_list, video_resolution[2], video_throttles, "Video")
+                    executor.submit(range_list, audio_resolution[2], audio_throttles, "Audio")
 
                 with ThreadPoolExecutor() as executor:
                     executor.submit(download_segments, video_throttles, video_stream, video_segment_list, video_file,
@@ -324,23 +247,17 @@ def youtube_gui():
                         mime="video/mp4"
                     )
                 os.remove(output_file)
-        except ValueError:
+        except RegexMatchError:
             st.error("Please Enter Valid Url!")
-            
-def segment_resolution(details):
-    for i in details["streamingData"]["adaptiveFormats"]:
-        try:
-            if "video" in i["mimeType"]:
-                raw_quality_list.append((i["qualityLabel"], i["url"], int(i["contentLength"])))
-                displaying_quality_list.append((i["qualityLabel"]))
-            if i["itag"]==139:
-                    default_lowest_audio.append((f"{round(int(i['bitrate']) / 1000)}kbps", i["url"], int(i["contentLength"]), i["mimeType"]))
-        except:
-            pass
-        
+
+def segment_resolution(yt):
+    streams=yt.streams.filter(only_video=True)
+    for stream in streams:
+        if stream.resolution not in Resolution_list and stream.resolution!=None:
+            Raw_rso_list.append([stream.resolution,stream.itag])
+            Resolution_list.append(stream.resolution)
+
 def Section_Download():
-    stream_2_url=None
-    audio_throttles=None
     st.title("Youtube Segment Trimmer ✂")
     col1, col2 = st.columns(2)
     with col1:
@@ -353,15 +270,12 @@ def Section_Download():
     if st.session_state.get("checked_3", False):
         if url:
             try:
-                video_id = get_video_id(url)
-                if video_id is None:
-                    raise ValueError("Invalid URL")
-                details = video_response_scraper(video_id)
-                duration = int(details["videoDetails"]["lengthSeconds"])
-                segment_resolution(details)
+                yt = YouTube(url)
+                duration = yt.length
+                segment_resolution(yt)
                 section_1, section_2 = st.columns(2)
                 with section_1:
-                    selected_resolution = st.selectbox("Available Resolutions:", displaying_quality_list)
+                    selected_resolution = st.selectbox("Available Resolutions:", Resolution_list)
                 with section_2:
                     audio = st.checkbox("With Audio")
 
@@ -404,12 +318,13 @@ def Section_Download():
                         st.info("Trim segment, start time must be less than end time!")
                     process = st.button("Process")
                     if process:
-                        stream_1 = raw_quality_list[displaying_quality_list.index(selected_resolution)][1]
-                        file_size = raw_quality_list[displaying_quality_list.index(selected_resolution)][2]
-                        stream_1_url = stream_1
+                        stream_1 = yt.streams.get_by_itag(Raw_rso_list[Resolution_list.index(selected_resolution)][1])
+                        file_size = stream_1.filesize
+                        stream_1_url = stream_1.url
                         if audio:
-                            stream_2_url = default_lowest_audio[0][1]
-                            file_size += default_lowest_audio[0][2]
+                            stream_2 = yt.streams.get_lowest_resolution()
+                            stream_2_url = stream_2.url
+                            file_size += stream_2.filesize
 
                         progress_bar = st.progress(0)
                         downloading_status = st.empty()
@@ -432,12 +347,12 @@ def Section_Download():
                             executor.submit(download_segments, video_throttles,stream_1_url, video_segment_list,
                                             video_file,
                                             file_size)
-                            output_video= "Video.mp4"
+                            output_file="Video.mp4"
                             if audio:
                                 executor.submit(download_segments, audio_throttles, stream_2_url, audio_segment_list,
                                             audio_file,
                                             file_size)
-                                output_video= "Downloaded.mp4"
+                                output_file="Downloaded.mp4"
                             while total_received < file_size:
                                 progress_bar.progress(min(int(percentage), 100))
                                 downloading_status.text(f"Processed:{percentage}%")
@@ -451,10 +366,10 @@ def Section_Download():
 
 
 
-                        if start < end <= duration:
+                        if start < end and end <= duration:
                             with st.spinner("Trimming video, please wait..."):
                                 subprocess.run([
-                                    'ffmpeg', '-y', '-ss', f'{start}', '-to', f'{end}', '-i', f'{output_video}',
+                                    'ffmpeg', '-y', '-ss', f'{start}', '-to', f'{end}', '-i', f'{output_file}',
                                     '-c', 'copy', '-avoid_negative_ts', '1', 'trimmed.mp4'
                                 ])
                         else:
@@ -467,16 +382,16 @@ def Section_Download():
                                 file_name=trimmed_file,
                                 mime="video/mp4"
                             )
-                        os.remove(output_video)
+                        os.remove(output_file)
                         os.remove(trimmed_file)
-            except ValueError:
+            except RegexMatchError:
                 st.error("Please enter a valid URL!")
             except TypeError:
                 st.error("Please enter a valid URL!")
 
         else:
             st.warning("Enter URL!")
-            
+
 def main():
     st.sidebar.title("Options:")
     select_option = st.sidebar.selectbox("Youtube_Options:", ["▶️ Video Download", "⬇️ Only Audio","✂ Segment Download"])
@@ -484,7 +399,7 @@ def main():
     if st.session_state.select_option == "▶️ Video Download":
         st.session_state.checked_1 = False
         st.session_state.checked_3 = False
-        youtube_gui()
+        Youtube_Gui()
     if st.session_state["select_option"] == "⬇️ Only Audio":
         st.session_state.checked_2 = False
         st.session_state.checked_3 = False
